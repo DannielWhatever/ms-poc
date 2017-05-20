85bf3ab0c06556c107be8f2ddc771f1b901aaa45
daniel gutierrez <daniel.gutierrezr@bci.cl>
2017-05-20 09:53:29 -0400

    :banana: :banana: :banana: hystrix

--- bff/books-client/src/main/java/poc/ms/bff/booksclient/BooksClientApplication.java
+++ bff/books-client/src/main/java/poc/ms/bff/booksclient/BooksClientApplication.java
@@ -1,11 +1,13 @@
 package poc.ms.bff.booksclient;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableFeignClients
 @SpringBootApplication
+@EnableCircuitBreaker
 public class BooksClientApplication {
 
 	public static void main(String[] args) {
@@ -37,10 +42,13 @@ class BffController{
 		this.apiBooks = apiBooks;
 	}
 
+	@HystrixCommand(fallbackMethod = "fallbackGetBookNames")
 	@GetMapping("/books/names")
 	public List<String> getBookNames(){
 		return this.apiBooks.getBooks().stream().map(b-> b.getName()).collect(Collectors.toList());
 	}
+
+	public List<String> fallbackGetBookNames() { return Collections.EMPTY_LIST; }
 
 }
 
