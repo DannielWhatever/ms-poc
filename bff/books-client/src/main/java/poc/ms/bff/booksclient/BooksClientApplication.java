ea1bda17aaa6c3c855e1f632d43602ebc3361361
daniel gutierrez <daniel.gutierrezr@bci.cl>
2017-05-20 09:43:32 -0400

    change structure directories :kissing_heart:

---
+++ bff/books-client/src/main/java/poc/ms/bff/booksclient/BooksClientApplication.java
@@ -0,0 +1,62 @@
+package poc.ms.bff.booksclient;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@EnableFeignClients
+@SpringBootApplication
+public class BooksClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksClientApplication.class, args);
+	}
+
+}
+
+@RestController
+class BffController{
+
+	private final ApiBooks apiBooks;
+
+	@Autowired
+	public BffController(ApiBooks apiBooks) {
+		this.apiBooks = apiBooks;
+	}
+
+	@GetMapping("/books/names")
+	public List<String> getBookNames(){
+		return this.apiBooks.getBooks().stream().map(b-> b.getName()).collect(Collectors.toList());
+	}
+
+}
+
+@FeignClient("ms-books")
+interface ApiBooks {
+	@RequestMapping(method = RequestMethod.GET, value = "/books")
+	List<Book> getBooks();
+}
+
+class Book {
+	private Integer id;
+	private String name;
+
+	public Book() {}
+
+	public Integer getId() { return id; }
+	public String getName() { return name; }
+
+}
---
+++ bff/books-client/src/test/java/poc/ms/bff/booksclient/BooksClientApplicationTests.java
@@ -0,0 +1,16 @@
+package poc.ms.bff.booksclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BooksClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
